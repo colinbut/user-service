a0c7297aec032ba36362a389e5f8fb318e8d6bd1
Colin But <colinbut@users.noreply.github.com>
2018-03-01 09:45:42 +0000

    insert into users cql

--- src/main/java/com/mycompany/user/dao/UserDaoImpl.java
+++ src/main/java/com/mycompany/user/dao/UserDaoImpl.java
@@ -27,6 +27,7 @@ public class UserDaoImpl implements UserDao {
     private static final String SELECT_ALL_USERS_QUERY = "SELECT * FROM userKS.USERS";
     private static final String SELECT_USER_WITH_SSN = "SELECT * FROM userKS.USERS where SSN = ";
     private static final String DELETE_USER = "DELETE FROM userKS.USERS where SSN = ";
+    private static final String INSERT_USER = "INSERT INTO userKS.USERS (ssn, forename, surname, dob, address, postcode, city, country) values ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')";
 
     private Cluster cluster = null;
     private Session session = null;
