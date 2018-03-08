0cc149adc5e2ef09493155f60dce915d90317ac4
Colin But <colinbut@users.noreply.github.com>
2018-03-08 22:13:46 +0000

    dto objects to be sent across wire

---
+++ src/main/java/com/mycompany/user/dto/UserDto.java
@@ -0,0 +1,57 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2018 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.user.dto;
+
+import java.util.Date;
+
+public class UserDto {
+
+    private String ssn;
+    private String firstname;
+    private String secondname;
+    private Date dob;
+    private AddressDto address;
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getSecondname() {
+        return secondname;
+    }
+
+    public void setSecondname(String secondname) {
+        this.secondname = secondname;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public AddressDto getAddress() {
+        return address;
+    }
+
+    public void setAddress(AddressDto address) {
+        this.address = address;
+    }
+}
